77b4bf1c8854df9204d1d85acdc4586c95fa8afa
Hiroshana <hiroshanawanni@gmail.com>
2025-06-12 20:43:51 +1000

    Created Sanity models

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,26 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import { blockContentType } from "./blockContentType";
+import { categoryType } from "./categoryType";
+import { productType } from "./productType";
+import { orderType } from "./orderType";
+import { bannerType } from "./bannerType";
+import { brandType } from "./brandType";
+import { blogType } from "./blogType";
+import { blogCategoryType } from "./blogCategoryType";
+import { authorType } from "./authorType";
+import { addressType } from "./addressType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [
+    categoryType,
+    blockContentType,
+    productType,
+    brandType,
+    orderType,
+    blogType,
+    blogCategoryType,
+    addressType,
+    bannerType,
+    authorType,
+  ],
+};
