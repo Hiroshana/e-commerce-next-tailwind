f4998e3c1fb9243e27155b2cd2de4dbcc829dedd
Hiroshana <hiroshanawanni@gmail.com>
2025-06-14 21:21:03 +1000

    Added popular category section

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,15 +1,21 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
+import HomeCategories from "@/components/HomeCategories";
 import ProductGrid from "@/components/ProductGrid";
+import { getCategories } from "@/sanity/queries";
 import React from "react";
 
-const Home = () => {
+const Home = async () => {
+  const categories = await getCategories(6);
+
+  console.log(categories);
   return (
     <Container>
       <HomeBanner />
       <div className="py-10">
         <ProductGrid />
       </div>
+      <HomeCategories categories={categories} />
     </Container>
   );
 };
---
+++ sanity/queries/index.ts
@@ -0,0 +1,19 @@
+import { sanityFetch } from "../lib/live";
+
+const getCategories = async (quantity?: number) => {
+  try {
+    const query = quantity
+      ? `*[_type == 'category'] | order(name asc) [0...$quantity]{...,"productCount" : count(*[_type == "product" && references(^._id)])}`
+      : `*[_type == 'category'] | order(name asc){...,"ProductCount":count(*[_type == "product" && references(^._id)])}`;
+    const { data } = await sanityFetch({
+      query,
+      params: quantity ? { quantity } : {},
+    });
+    return data;
+  } catch (error) {
+    console.log("Error fetching categories", error);
+    return [];
+  }
+};
+
+export { getCategories };
